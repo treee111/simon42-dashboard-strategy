1e789a79eb9e2434187289b144cf9c1838070b7b
Benjamin Kreuscher <benjamin.kreuscher@mercedes-benz.com>
2026-04-17 00:40:55 +0200

    do not version dist/ dir in Github

--- dist/simon42-dashboard-strategy-598.c4475779.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksimon42_dashboard_strategy=self.webpackChunksimon42_dashboard_strategy||[]).push([[217,598],{217(s,e,a){a.d(e,{G:()=>r});const r=["overview","custom_cards","areas","weather","energy"]}}]);
